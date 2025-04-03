0a3fa29173f4cbac7b5f8e75f65c4f039d86f4d7
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-04-03 20:48:45 +0300

    add createPortal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import {Cart} from './components/Cart/Cart.tsx';
+import {Header} from "./components/Header/Header.tsx";
 
 function App() {
-    return <Cart />
+    return <Header />
 }
 
 export default App
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import s from './Modal.module.css'
 import {ReactNode} from 'react';
+import {createPortal} from "react-dom";
 
 type Props = {
     open: boolean
@@ -8,21 +9,26 @@ type Props = {
     children: ReactNode
 }
 
-export const Modal = ({ onClose, open, title, children }: Props) => {
+export const Modal = ({onClose, open, title, children}: Props) => {
     return (
         <>
-            {open && (
-                <div className={s.overlay}>
-                    <div className={s.content}>
-                        <h3 className={s.title}>{title}</h3>
-                        <hr />
-                        {children}
-                        <button className={s.closeIcon} onClick={onClose}>
-                            x
-                        </button>
-                    </div>
-                </div>
-            )}
+            {open &&
+                <>
+                    {createPortal(
+                        <div className={s.overlay}>
+                            <div className={s.content}>
+                                <h3 className={s.title}>{title}</h3>
+                                <hr/>
+                                {children}
+                                <button className={s.closeIcon} onClick={onClose}>
+                                    x
+                                </button>
+                            </div>
+                        </div>,
+                        document.body)}
+                </>
+
+            }
         </>
     )
 }
