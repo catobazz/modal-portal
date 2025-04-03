edf3f6be3d3dd91ce9d258b82e97d812149c74e5
Artem Rybin <catobazzz@gmail.com>
2025-04-03 14:27:46 +0300

    add children

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,22 @@
 import s from './Modal.module.css'
+import {ReactNode} from 'react';
 
 type Props = {
     open: boolean
     onClose?: () => void
+    title: string
+    children: ReactNode
 }
 
-export const Modal = ({ onClose, open }: Props) => {
+export const Modal = ({ onClose, open, title, children }: Props) => {
     return (
         <>
             {open && (
                 <div className={s.overlay}>
                     <div className={s.content}>
-                        <h3 className={s.title}>Cart</h3>
+                        <h3 className={s.title}>{title}</h3>
                         <hr />
-                        <ul>
-                            <li>1 товар</li>
-                            <li>2 товар</li>
-                            <li>3 товар</li>
-                        </ul>
+                        {children}
                         <button className={s.closeIcon} onClick={onClose}>
                             x
                         </button>
