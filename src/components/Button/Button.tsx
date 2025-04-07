85671f8dde2f1eb1b96a5bd8f28a4fa4bc736eed
Artem Rybin <catobazzz@gmail.com>
2025-04-07 15:06:27 +0300

    add Slot for polymorphic Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,17 @@
 import {ComponentPropsWithoutRef} from 'react'
 import s from './Button.module.css'
 import clsx from 'clsx';
+import {Slot} from '@radix-ui/react-slot';
 
 type Props = {
     variant?: 'primary' | 'secondary' | 'outlined'
+    asChild?: boolean
 } & ComponentPropsWithoutRef<'button'>
 
-export const Button = ({variant = 'primary', className, ...rest}: Props) => {
+export const Button = ({asChild, variant = 'primary', className, ...rest}: Props) => {
+    const Component = asChild ? Slot : 'button';
     return (
-        <button
+        <Component
             className={clsx(s.button, s[variant], className)}
             {...rest}
         />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import {Cart} from '../Cart/Cart.tsx';
 import s from './Header.module.css'
+import {Button} from '../Button/Button.tsx';
 
 export const Header = () => {
     return (
         <div className={s.headerWrapper}>
             <div className={s.container}>
                 <h3>logotype</h3>
-                <Cart />
+                <Cart/>
+                <Button asChild>
+                    <a href="/sign-up">Sign Up</a>
+                </Button>
             </div>
         </div>
     )
