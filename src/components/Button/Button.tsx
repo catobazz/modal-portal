064edbab99a468e0ef604c8a2ecd45c2fd817f18
Artem Rybin <catobazzz@gmail.com>
2025-04-07 14:31:15 +0300

    add clsx for Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import {ComponentPropsWithoutRef} from 'react'
 import s from './Button.module.css'
+import clsx from 'clsx';
 
 type Props = {
     variant?: 'primary' | 'secondary' | 'outlined'
@@ -8,12 +9,7 @@ type Props = {
 export const Button = ({variant = 'primary', className, ...rest}: Props) => {
     return (
         <button
-            className={`${s.button}
-  ${variant === 'primary' ? s.primary : ''}
-  ${variant === 'secondary' ? s.secondary : ''}
-  ${variant === 'outlined' ? s.outlined : ''}
-  ${className}
-     `}
+            className={clsx(s.button, s[variant], className)}
             {...rest}
         />
     )
